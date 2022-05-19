1cb4cb4d20efac5b4b2f15302b449e8b6766684c
Giannina Binder <giannina@haldenweg.ch>
2022-05-19 17:11:38 +0200

    the watch list is now working not yet styled

---
+++ app/models/movie.rb
@@ -0,0 +1,6 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  has_many :lists, through: :bookmarks
+
+  validates :overview, :title, uniqueness: true, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Movie.create(
+  title: "Backseat",
+  overview: "We follow a dog sitting blissfully ignorant in the backseat of a getaway car while it's owner is taking part in a gas station robbery.",
+  poster_url: "https://wiki.animation-luzern.ch/images/thumb/5/58/Backseat_Still_comped.jpg/1600px-Backseat_Still_comped.jpg",
+  rating: 9.0
+)
+
+Movie.create(
+  title: "Spiderman Homecoming",
+  overview: "Peter Parker balances his life as an ordinary high school student in Queens with his superhero alter-ego Spider-Man, and finds himself on the trail of a new menace prowling the skies of New York City.",
+  poster_url: "https://de.web.img2.acsta.net/pictures/17/05/26/14/52/070063.jpg",
+  rating: 8.0
+)
+
+Movie.create(
+  title: "the Lord of the Rings",
+  overview: "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
+  poster_url: "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg",
+  rating: 9.9
+)
+
+Movie.create(
+  title: "the batman",
+  overview: "When a sadistic serial killer begins murdering key political figures in Gotham, Batman is forced to investigate the city's hidden corruption and question his family's involvement.",
+  poster_url: "https://hitparade.ch/movieimages/76507-1.jpg",
+  rating: 6.9
+)
+
+Movie.create(
+  title: "Guardians of the Galaxy",
+  overview: "A group of intergalactic criminals must pull together to stop a fanatical warrior with plans to purge the universe.",
+  poster_url: "https://image.api.playstation.com/vulcan/ap/rnd/202106/0215/Pw9cWnyqkix3EoCOGqrN1cgN.png",
+  rating: 7.9
+)
